6f622f6501ce4eac1f4f844e04a16a1ac576c632
Guillaume Lederrey <guillaume.lederrey@gmail.com>
2013-12-16 23:52:49 +0100

    initial commit

---
+++ src/test/java/com/github/javadojo/MarsRoverTest.java
@@ -0,0 +1,65 @@
+package com.github.javadojo;
+
+import org.junit.Test;
+import static org.hamcrest.CoreMatchers.*;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+public class MarsRoverTest {
+
+    @Test
+    public void driveEast() {
+        MarsRover rover = new MarsRover("s");
+        assertThat("X*", equalTo(rover.path()));
+    }
+
+    @Test
+    public void driveEastForABitLonger() {
+        MarsRover rover = new MarsRover("ssss");
+        assertThat("X---*", equalTo(rover.path()));
+    }
+
+    @Test
+    public void driveNorth() {
+        MarsRover rover = new MarsRover("lsss");
+        String expectedPath = new StringBuilder()
+                .append("*")
+                .append("|")
+                .append("|")
+                .append("X")
+                .toString();
+        assertThat(expectedPath, equalTo(rover.path()));
+    }
+
+    @Test
+    public void driveEstThanTurnLeft() {
+        MarsRover rover = new MarsRover("rsssslssss");
+        String expectedPath = new StringBuilder()
+                .append("    *")
+                .append("    |")
+                .append("    |")
+                .append("    |")
+                .append("X---+")
+                .toString();
+        assertThat(expectedPath, equalTo(rover.path()));
+    }
+
+    @Test
+    public void driveNorthAfterInitialProgrammingToTheEast() {
+        MarsRover rover = new MarsRover("ssss")
+                .turnLeft()
+                .moveForward()
+                .moveForward();
+        String expectedPath = new StringBuilder()
+                .append("    *")
+                .append("    |")
+                .append("X---+")
+                .toString();
+        assertThat(expectedPath, equalTo(rover.path()));
+    }
+
+    @Test
+    public void driveEastThanTakeSampleThanDriveABitMore() {
+        MarsRover rover = new MarsRover("sssSsss");
+        assertThat("X--S--*", equalTo(rover.path()));
+    }
+}
