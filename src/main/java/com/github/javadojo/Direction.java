e6ddf5487ea9ca251bbc81b16ac3bf4717d6d164
Mohamed Aïmane ELKIRA <NNElkiraMo@nestle.com>
2014-01-23 01:44:41 +0000

    Enum has already ordinal().. stupid me !

--- src/main/java/com/github/javadojo/Direction.java
+++ src/main/java/com/github/javadojo/Direction.java
@@ -2,45 +2,39 @@ package com.github.javadojo;
 
 enum Direction {
 
-    EAST(0) {
+    EAST {
         @Override
         void doMoveForward(DisplayGrid displayGrid) {
             displayGrid.moveHorizentally(true);
         }
     },
-    NORTH(1) {
+    NORTH {
         @Override
         void doMoveForward(DisplayGrid displayGrid) {
             displayGrid.moveVertically(true);
         }
     },
-    WEST(2) {
+    WEST {
         @Override
         void doMoveForward(DisplayGrid displayGrid) {
             displayGrid.moveHorizentally(false);
         }
     },
-    SOUTH(3) {
+    SOUTH {
         @Override
         void doMoveForward(DisplayGrid displayGrid) {
             displayGrid.moveVertically(false);
         }
     };
 
-    private final int order;
-
-    private Direction(int order) {
-        this.order = order;
-    }
-
     Direction turnLeft(DisplayGrid displayGrid) {
         displayGrid.turn();
-        return valueOf(order + 1);
+        return next();
     }
 
     Direction turnRight(DisplayGrid displayGrid) {
         displayGrid.turn();
-        return valueOf(order - 1);
+        return previous();
     }
 
     Direction moveForward(DisplayGrid displayGrid) {
@@ -50,12 +44,19 @@ enum Direction {
 
     abstract void doMoveForward(DisplayGrid displayGrid);
 
-    private Direction valueOf(int order) {
-        order = ((order % 4) + 4) % 4;
-        for (Direction direction : values()) {
-            if (direction.order == order)
-                return direction;
-        }
-        throw new IllegalArgumentException("out of bound.");
+    private Direction next() {
+        int nextOrder = ordinal() + 1;
+        return valueOf(nextOrder);
     }
+
+    private Direction previous() {
+        int previousOrder = ordinal() - 1;
+        return valueOf(previousOrder);
+    }
+
+    private Direction valueOf(int order) {
+        int orderInRange = ((order % 4) + 4) % 4;
+        return values()[orderInRange];
+    }
+
 }
